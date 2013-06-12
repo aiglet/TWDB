964c2f4bead06c8000e57f3149d124e0b5f5feee
Kate Secor <aiglet@gmail.com>
2013-06-11 19:44:07 -0700

    HTML WTDB
    The Web Triggers Database in HTML format.

---
+++ HTML version/getinfo.php
@@ -0,0 +1,22 @@
+
+
+<html>
+<head>
+<title>Get Info</title>
+</head>
+
+<body>
+<!-- Fill in this with better stuff later -->
+<?php
+
+function processISBNQuery() {
+  $isbn = $_GET[isbn]
+  $url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" . $isbn
+  header("Book Data: $url");
+  exit;
+} 
+?>
+
+</body>
+</html
+
