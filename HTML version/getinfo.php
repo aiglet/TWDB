25fbe3d030abf1e49390e59853dfccc205537526
Kate Secor <aiglet@gmail.com>
2014-01-29 12:16:12 -0800

    Update query
    This actually works now.

--- HTML version/getinfo.php
+++ HTML version/getinfo.php
@@ -11,7 +11,7 @@
 
 function processISBNQuery() {
   $isbn = $_GET[isbn]
-  $url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" . $isbn
+  $url = "https://www.googleapis.com/books/v1/volumes?q=" . $isbn
   header("Book Data: $url");
   exit;
 } 
